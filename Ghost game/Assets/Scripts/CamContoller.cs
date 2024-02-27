396d9a8ae32d9b5978f7669cd3e4e3e31968c56a
kellenstrydom <131270586+kellenstrydom@users.noreply.github.com>
2024-02-27 11:53:09 +0200

    cam

---
+++ Ghost game/Assets/Scripts/CamContoller.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class CamContoller : MonoBehaviour
+{
+    public Transform focus;
+    public Vector2 followOffset;
+
+    private void Update()
+    {
+        Vector2 offset = Vector2.zero;
+        bool isOutsideRange = false;
+        
+        
+        
+        if (Math.Abs(focus.position.x - transform.position.x) > followOffset.x)
+        {
+            
+            if (focus.position.x > transform.position.x)
+            {
+                offset.x = focus.position.x - (transform.position.x + followOffset.x);
+            }
+            else
+            {
+                offset.x = (transform.position.x - followOffset.x) + focus.position.x;
+            }
+            
+            isOutsideRange = true;
+        }
+        
+        if (Math.Abs(focus.position.y - transform.position.y) > followOffset.y)
+        {
+            if (focus.position.y > transform.position.y)
+            {
+                offset.y = focus.position.y - (transform.position.y + followOffset.y);
+            }
+            else
+            {
+                offset.y = (transform.position.y - followOffset.y) + focus.position.y;
+            }
+
+            isOutsideRange = true;
+        }
+
+        if (isOutsideRange)
+        {
+            transform.position += (Vector3)offset;
+        }
+
+    }
+}
